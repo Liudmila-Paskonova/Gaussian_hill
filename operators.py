e1d0d7bcc5660129df5c3aa3225e900c4867f2cd
Liudmila Paskonova <paskonovalv@gmail.com>
2022-10-30 23:28:56 +0300

    Update operators.py

--- operators.py
+++ operators.py
@@ -13,23 +13,6 @@ def calc_curl(u, v, domain, diff_method):
     return -diff_method(u, 'y', domain) + diff_method(v, 'x', domain)
 
     
-def central_diff2(f, direction, domain):
-    out = np.empty_like(f)
-    if direction == 'y':
-        for j in range(1, domain.ny):
-            out[j, :] = (f[j + 1, :] - f[j - 1, :]) / 2.0 / domain.dy
-        out[-1, :] = (f[1, :] - f[-2, :]) / 2.0 / domain.dy
-        out[0, :] = out[-1, :]
-    elif direction == 'x':
-        for i in range(1, domain.nx):
-            out[:, i] = (f[:, i + 1] - f[:, i - 1]) / 2.0 / domain.dx
-        out[:, -1] = (f[:, 1] - f[:, -2]) / 2.0 / domain.dx
-        out[:, 0] = out[:, -1]
-    else:
-        raise Exception(f"Error in central_diff2. Wrong direction value {direction}!")
-    return out
-
-    
 def central_diff4(f, direction, domain):
     diff_f = np.empty_like(f)
     if direction == 'y':
